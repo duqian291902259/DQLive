89a38a5854b58489246d663892669f359445b27b
杜小菜 <duqian2010@gmail.com>
2024-05-09 13:57:31 +0800

    *【so】Hello from c++,DQ

--- nativelib/src/main/cpp/nativelib.cpp
+++ nativelib/src/main/cpp/nativelib.cpp
@@ -5,6 +5,6 @@ extern "C" JNIEXPORT jstring JNICALL
 Java_com_duqian_nativelib_NativeLib_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+    std::string hello = "Hello from C++,DQ";
     return env->NewStringUTF(hello.c_str());
 }
